2d9e3f29c5881702960fac6c728b2e5dbae5cff0
Talal Ahmad <talaljalil164@gmail.com>
2026-05-31 21:57:53 +0500

    changes

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -23,6 +23,9 @@ const Navbar = () => {
     }
 
     useEffect(() => {
+        if (!session) {
+            router.push('/')
+        }
         if (status === "authenticated") {
             getData()
         }
